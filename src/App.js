1b4c744a57870a17121b2b2bd7c7a980558ba118
phutadonza <135265650+phutadonza@users.noreply.github.com>
2024-01-08 10:26:54 +0700

    create component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import './style.css'
+import { Header } from './func-components';
 
 function App() {
   return (
-    <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
-    </>
+    <Header/>
   )
 }
 
