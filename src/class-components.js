0d55aefee75ec27c65e662af6b12183c5cf54d12
phutadonza <135265650+phutadonza@users.noreply.github.com>
2024-01-08 11:33:39 +0700

    Update class-components.js

--- src/class-components.js
+++ src/class-components.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react"
 
-export default class Calendar extends React.Component{
+export default class Calendar extends Component{
     getDate(){
         const dayNames = ['อาทิตย์','จันทร์','อังคาร','พุธ',
                           'พฤหัส','ศุกร์','เสาร์']
@@ -10,13 +10,13 @@ export default class Calendar extends React.Component{
                             'ตุลาคม','พฤศจิกายน','ธันวาคม']
         const date = new Date()
         const weekDay = dayNames[date.getDay()]
-        const day = date.getData()
+        const day = date.getDate()
         const month = monthNames[date.getMonth()]
         const year = date.getFullYear() + 543
-        return 'วัน${weekDay}ที่ ${day}${month}${year}'
+        return `วัน${weekDay}ที่ ${day}${month}${year}`
     }
 
     render(){
-        return <div>{Calendar}</div>
+        return <div>{this.getDate()}</div>
     }
 }
