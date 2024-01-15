d0c832347190a4830d2669db0a8a990fc7a9a213
phutadonza <phutadol5627@gmail.com>
2024-01-15 10:09:35 +0700

    modify class-comp
    modify component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Header,Content,Footer } from './func-components';
-import Calendar from './class-components';
+import Button from './class-components';
 
 function App() {
   return (
     <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
+      <Button/>
     </>
   )
 }
