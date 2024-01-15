009d83f42feec02eb6da29bcc58f87d5cd39dc2c
phutadonza <phutadol5627@gmail.com>
2024-01-15 11:33:28 +0700

    create eventdata2
    create function eventdata2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Header,Content,Footer } from './func-components';
+//import { Header,Content,Footer } from './func-components';
 import Button from './class-components';
+import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
 
 function App() {
   return (
     <>
       <Button/>
+      <Calculator2/>
+      <EventData2/>
     </>
   )
 }
