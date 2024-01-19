13893ca84af37a46cdfc2fa502020031b8573b16
suparin13 <152370501+suparin13@users.noreply.github.com>
2024-01-19 15:37:29 +0700

    create MessageBox
    create MessageBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import RefsFunc from './refs-func';
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
   return (
     <>
       <RefsArray/>
+      <MessageBox/>
     </>
   )
 }
