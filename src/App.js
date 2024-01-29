fa372054a37152ce141e41debd73a5ebe176d844
phutadonza <phutadol5627@gmail.com>
2024-01-29 11:29:42 +0700

    create header2 and context2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { userContext } from './context';
-import Content from './context-content';
+import Content2 from './context-content2';
+import Header2 from './context-header2';
+
 
 function App() {
+  let[user,setUser] = React.useState('');
   return (
-    <userContext.Provider value={'Phutadon Phummarin'}>
-      <Content/>
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
     </userContext.Provider>
   )
 }
