f7d1ab9af5d0bece587434252d108d11d7361059
phutadonza <phutadol5627@gmail.com>
2024-03-04 12:10:31 +0700

    create form validation
    create form validation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import FormCheck from "./form-check";
+import FormValidation from "./form-validation";
 
 
 
 export default function App() {
-  return <FormCheck />;
+  return <FormValidation />;
 }
